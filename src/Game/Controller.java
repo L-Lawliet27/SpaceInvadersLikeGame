52cb5bf78a257e287d91078b3f3b171c4fd2735c
Andres Salazar Colina <andressalazarcolina@gmail.com>
2020-07-29 21:06:29 +0200

    Controller Exceptions

--- src/Game/Controller.java
+++ src/Game/Controller.java
@@ -20,8 +20,7 @@ public class Controller
         gamePrinter = PrinterTypes.BOARDPRINTER.getObject(game);
     }
 
-    public void run()
-    {
+    public void run() throws FileContentsException, SuperMissileException, IOException, NoShockwaveException, MissileInFlightException, OffWorldException {
         System.out.println(gamePrinter);
         while (!game.isFinished()){
             System.out.println("Command > ");
@@ -34,8 +33,13 @@ public class Controller
 
                 } else throw new CommandParseException("Invalid Command");
 
-            } catch (CommandParseException | CommandExecuteException | SuperMissileException | NoShockwaveException | OffWorldException | MissileInFlightException | IOException | FileContentsException ex){
+            } catch (CommandParseException | CommandExecuteException ex){
                 System.err.format(ex.getMessage() + "%n%n");
+                Throwable cause = ex.getCause();
+                if(cause != null){
+                    System.err.println("Cause of Exception:");
+                    System.err.println(" " + ex.getCause());
+                }
             }
         }
         System.out.println(game.getWinnerMessage());
@@ -43,32 +47,6 @@ public class Controller
 
 
     }//run
-
-//        boolean exit = false;
-//
-//        while (!exit) {
-//
-//
-//            System.out.println();
-//
-//            String[] words = input.nextLine().toLowerCase().trim().split("\\s+");
-//
-//            Command com = CommandGenerator.parse(words);
-//
-//            if (com != null)
-//            {
-//                if(com.getName().equals("exit")) exit = true;
-//
-//                else if (com.execute(game)) System.out.println(game);
-//
-//                else System.out.format();
-//            }
-//
-//        }// while
-//
-//        System.out.println("GM.Game Exited");
-
-
 
 
 
